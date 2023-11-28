a2843788aac477112e14406bcfaa1a3d642bb669
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2023-11-29 00:03:53 +0300

    print fizzer

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
-    if i % 2 == 0:
-        diff = 0
-    else:
-        diff = 32
-    print('{}'.format(chr(i - diff)), end='')
-    
+for i in range(122, 96, -1):
+    print("{:c}".format(i), end='')
+    print("{:c}".format(i - 32), end='')
+ 
