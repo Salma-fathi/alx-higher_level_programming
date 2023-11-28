59f1e32d7ee58d66a804b257581b0f57074c92ab
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2023-11-29 01:23:17 +0300

    else looop

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z') + 1):
-    if chr(i) != 'q' and chr(i) != 'e':
-        print(chr(i), end='')
-print()
+    if chr(i) == 'e' or chr(i) == 'q':
+        continue
+    else:
+        print("{:s}".format(chr(i)), end="")
+        
