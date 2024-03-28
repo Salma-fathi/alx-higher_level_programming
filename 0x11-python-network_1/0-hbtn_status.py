ce3f5aeb80fa97a87b41d41a51e35dffe3690e02
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2024-03-29 01:28:27 +0300

    add all task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,3 +5,8 @@
 if __name__ == '__main__':
     import urllib.request
     url = 'https://alx-intranet.hbtn.io/status'
+    with  urllib.request.urlopen(url) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
