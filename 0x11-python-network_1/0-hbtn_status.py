5da4fbbc5b55b528763870ea4bd318cac6f664d4
Salma-fathi <salma89040@gmail.com>
2024-03-28 15:01:42 -0700

    full

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""A python script that fetches https://alx-intranet.hbtn.io/status"""
+
+if __name__ == '__main__':
+    import urllib.request
+    url = 'https://alx-intranet.hbtn.io/status'
